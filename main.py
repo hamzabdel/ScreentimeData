4459d7d90bef98fe604e14afaf202a6d811b2635
hamzabdel <hamzaabdel094@gmail.com>
2025-04-05 01:29:48 -0700

    learning tensorflow

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 import seaborn as sns
+import tensorflow as tf
+from sklearn.preprocessing import StandardScaler
+from tensorflow.keras import layers, models
 
 df = pd.read_csv('C:/Users/hamza/OneDrive/Desktop/GitHub Projects/Neural Network/DiabetesPrediction/diabetes_prediction_dataset.csv')
 
-np_array = df.to_numpy()
+X = df.drop('Diabetes', axis=9)
+y = df['Diabetes']
