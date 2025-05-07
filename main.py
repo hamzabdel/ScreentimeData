61e7bfd7db2df12db64ce13921f60588a90f280d
hamzabdel <hamzaabdel@csu.fullerton.edu>
2025-05-07 13:55:22 -0700

    data eng

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
-import json
-import math
-from pathlib import Path
-
-import numpy as np
-import onnx
 import pandas as pd
-import pytorch_lightning as pl
-import tensorflow as tf
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torchmetrics
-import wandb
-from kaggle_secrets import UserSecretsClient
-from onnx_tf.backend import prepare
-from pytorch_lightning.callbacks import EarlyStopping
-from pytorch_lightning.callbacks import LearningRateMonitor
-from pytorch_lightning.loggers import WandbLogger
-from sklearn.model_selection import StratifiedGroupKFold
-from timm.optim import create_optimizer_v2
-from torchmetrics import MetricCollection
+
+df = pd.read_csv('digital_diet_mental_health.csv')
+
+df = df.drop_duplicates()
+df['total_device_hours'] = df['phone_usage_hours'] + df['laptop_usage_hours'] + df['tablet_usage_hours'] + df['tv_usage_hours']
+
+#wellness index on scale of 0-100
+df['wellness_index'] = (
+    (df['sleep_quality'] / 10 * 30) + 
+    (df['physical_activity_hours_per_week'] / 10 * 30) + 
+    ((10 - df['stress_level']) / 10 * 20) + 
+    (df['mindfulness_minutes_per_day'] / 30 * 20)
+)
+
+#creates age groups to help with visuals
+df['age_group'] = pd.cut(df['age'], bins=[0,25,35,50,100], labels=['18-25', '26-35', '36-50', '50+'])
+
+print(df.columns)
